20c8dd2a3d5282bd5992d450541f382416786f9e
Josimar Renepont <jnrenepont@hotmail.com>
2023-01-24 22:47:27 -0300

    category memory

---
+++ src/main/java/com/josimarrenepont/myproject/MyprojectApplication.java
@@ -0,0 +1,30 @@
+package com.josimarrenepont.myproject;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.josimarrenepont.myproject.entities.Category;
+import com.josimarrenepont.myproject.repositories.CategoryRepository;
+
+@SpringBootApplication
+public class MyprojectApplication implements CommandLineRunner {
+
+	@Autowired
+	private CategoryRepository categoryRepository;
+	public static void main(String[] args) {
+		SpringApplication.run(MyprojectApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+	
+		Category cat1 = new Category(1L, "Electronics");
+		Category cat2 = new Category(2L, "Books");
+		
+		categoryRepository.save(cat1);
+		categoryRepository.save(cat2);
+	}
+
+}
